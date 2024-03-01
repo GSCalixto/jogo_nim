825da57c6383f627cefb07c46189518f24f7e184
THEVOL <gabriel_calixto.gsc@hotmail.com>
2024-03-01 18:30:05 -0300

    concertando erros

--- jogo_nim.py
+++ jogo_nim.py
@@ -1,23 +1,41 @@
+usuario = 0
+computador = 0
+
 def computador_escolhe_jogada(n, m):
     computadorRemove = 1
+    
+    if n == m:
+        computadorRemove = m
 
-    while computadorRemove != m:
-        if (n - computadorRemove) % (m+1) == 0:
-            return computadorRemove
-        else:
-            computadorRemove += 1
+        return computadorRemove
+    else:
+        while computadorRemove != m:
+            if (n - computadorRemove) % (m+1) == 0:
+                return computadorRemove
+            else:
+                computadorRemove += 1
 
     return computadorRemove
+
 
 def usuario_escolhe_jogada(n, m):
     jogada = int(input('Quantas peças você vai tirar? '))
 
-    if jogada >= m and jogada <= n:
+    if jogada <= m and jogada <= n and jogada != 0:
         n = n - jogada
         return jogada
     else:
         print('Oops! Jogada inválida! Tente de novo.')
-        usuario_escolhe_jogada(n, m)       
+        usuario_escolhe_jogada(n, m)
+
+def verifica_ganhador(n, pcJogou):
+    global computador
+
+    if n == 0 and pcJogou == True:
+        computador =+ 1
+        print("Fim do jogo! O computador ganhou!")
+    elif n== 0 and pcJogou == False:
+        print("Fim de jogo! O usuário ganhou!")  
 
 def campeonato():
     numeroRodada = 1
@@ -30,13 +48,13 @@ def campeonato():
     print('\nPlacar: Você 0 X 3 Computador')
 
 def partida():
-    n = int(input('\nPor favor defina quantas peças terão na mesa '))
-    m = int(input('Defina o mínimo de peças retiradas a cada jogada '))
+    n = int(input('\nQuantas peças?'))
+    m = int(input('Limite de peças por jogada?'))
 
     vezDoPc = False
 
-    if n % (m+1):
-        print('\nUsuário começa!')
+    if n % (m+1) == 0:
+        print('\nVocê começa!')
     else:
         print('\nComputador começa!')
         vezDoPc = True
@@ -47,6 +65,7 @@ def partida():
             n = n - computadorRemove
 
             print('\nO computador tirou', computadorRemove, 'peças')
+            verifica_ganhador(n, vezDoPc)
 
             vezDoPc = False
         else:
@@ -54,6 +73,7 @@ def partida():
             n = n - jogadorRemove
 
             print('\nVocê tirou', jogadorRemove, 'peças')
+            verifica_ganhador(n, vezDoPc)
                     
             vezDoPc = True
             
@@ -64,22 +84,15 @@ def partida():
             if n > 0:
                 print('Agora restam,', n, 'peças no tabuleiro.\n')
 
-        if n <= m:
-            print("O jogo terminou!")
-            break
 
-def main():
-    print('Bem-vindo ao jogo do NIM! Escolha: \n')
+print('Bem-vindo ao jogo do NIM! Escolha: \n')
 
-    tipoDePartida = int(input('1 - para jogar uma partida\n2 - para jogar um campeonato \n'))
+tipoDePartida = int(input('1 - para jogar uma partida\n2 - para jogar um campeonato \n'))
 
-    if tipoDePartida == 2:
-        print('Voce escolheu um campeonato!\n')
-        campeonato()
-    elif tipoDePartida == 1:
-        partida()
-    else:
-        print('Opção inválida, escolha apenas entre "1" e "2"')
-
-if __name__ == "__main__":
-    main()
+if tipoDePartida == 2:
+    print('Voce escolheu um campeonato!\n')
+    campeonato()
+elif tipoDePartida == 1:
+    partida()
+else:
+    print('Opção inválida, escolha apenas entre "1" e "2"')
